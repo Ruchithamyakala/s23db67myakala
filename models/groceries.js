487762ea4d24b6728c5b37ae31888c4d599cbaca
Ruchithamyakala <143020768+Ruchithamyakala@users.noreply.github.com>
2023-11-29 20:20:05 -0600

    added files

--- models/groceries.js
+++ models/groceries.js
@@ -2,7 +2,17 @@
 const mongoose = require("mongoose")
 const groceriesSchema = mongoose.Schema({
 groceries_name: String,
-groceries_quantity: String,
-groceries_cost:Number,
+groceries_quantity: {
+  type: Number,
+  required: true,
+  min: 1.5,
+  max: 6,
+},
+groceries_cost:{
+    type: Number,
+    required: true,
+    min: 100,
+    max: 1000,
+  },
 })
 module.exports = mongoose.model("groceries",groceriesSchema)
--- routes/groceries.js
+++ routes/groceries.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const groceries_controllers = require('../controllers/groceries')
+const passport = require('passport');
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,13 +18,13 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET detail groceries page */
-router.get('/detail', groceries_controllers.groceries_view_one_Page);
+router.get('/detail',secured, groceries_controllers.groceries_view_one_Page);
 
 /* GET create groceries page */
-router.get('/create', groceries_controllers.groceries_create_post);
+router.get('/create',secured, groceries_controllers.groceries_create_post);
 
 /* GET create update page */
-router.get('/update', groceries_controllers.groceries_update_Page);
+router.get('/update',secured, groceries_controllers.groceries_update_Page);
 
 /* GET delete groceries page */
 router.get('/delete', groceries_controllers.groceries_delete_Page);
